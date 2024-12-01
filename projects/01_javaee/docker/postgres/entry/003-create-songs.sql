08effe86e17c887f1c7562cbb531a4ddaa45cb72
Niatomi <playervoker@gmail.com>
2024-12-02 01:27:59 +0400

    Restructured all schema

--- projects/01_javaee/docker/postgres/entry/003-create-songs.sql
+++ projects/01_javaee/docker/postgres/entry/003-create-songs.sql
@@ -4,11 +4,17 @@ CREATE TABLE public.songs
 (
     id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
     artist_id BIGINT NOT NULL,
+    album_id BIGINT NOT NULL,
     name character varying(100) NOT NULL,
     listen_count bigint NOT NULL DEFAULT 0,
     PRIMARY KEY (id),
     FOREIGN KEY (artist_id)
         REFERENCES public.artists (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    FOREIGN KEY (album_id)
+        REFERENCES public.albums (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
